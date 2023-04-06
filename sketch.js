54c4c370c7926d112eaec2afd27495e10f2f3e38
carolinewaxman <90156884+carolinewaxman@users.noreply.github.com>
2023-04-06 14:00:35 -0400

    changed some colors

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#0988e8');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#fa6c07');
   }
   updateSnakeCoordinates();
   updateHandContainer();
