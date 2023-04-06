d7dd63fa3346917eca5ab0f54eaded0a067933d0
Sonny <67891671+snguyen3@users.noreply.github.com>
2023-04-06 14:10:07 -0400

    Feat: Change snake and button colors

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#AA2BA5');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#FFFFFF');
   }
   updateSnakeCoordinates();
   updateHandContainer();
