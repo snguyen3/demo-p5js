26da2ecee1777803d37875cbd349f54b3472894e
gabecolmenares <gabriel.colmenares@vk.ai>
2023-04-06 14:05:44 -0400

    color blind friendly

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#170252');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#ffffff');
   }
   updateSnakeCoordinates();
   updateHandContainer();
